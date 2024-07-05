3cd41e70209ec34529473c83c6dd3551b35a73db
shuiyuechangchun <48248824+shuiyuechangchun@users.noreply.github.com>
2024-07-06 04:02:59 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,3 +16,4 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+svn co https://github.com/vernesong/OpenClash/tree/dd5e07d891f12ab46830db4064f7a0e19a08f38d/luci-app-openclash package/luci-app-openclash
